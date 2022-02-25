56e4d015bb9308ee516f8e3dc26215ba88cbd9ca
TheAngelFirst <58752128+TheAngelFirst@users.noreply.github.com>
2022-02-25 20:18:31 +0300

    delete config.py

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
+# create config.py
+
+# telegram bot token
 TOKEN = "5162474698:AAHeucw1j2t42VJYPhSdysNjG3xUEvn72hg"
 
+# 'currency_name': 'currency code'
 keys = {
     'биткоин': 'BTC',
     'эфириум': 'ETH',
