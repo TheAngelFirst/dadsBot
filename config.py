3729d076e8fbcfab4fff8b1cce8336cf1c452701
TheAngelFirst <58752128+TheAngelFirst@users.noreply.github.com>
2022-02-25 22:21:33 +0300

    add config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+# telegram bot token
+TOKEN = os.environ['TG_BOT_TOKEN']
+
+# 'currency_name': 'currency code'
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB'
+
+}
